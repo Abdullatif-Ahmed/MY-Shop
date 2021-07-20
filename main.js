2d778208dd0dad50cb721747432374d416466dfc
Abdullatif-Ahmed <apdoahmed795@gmail.com>
2021-07-20 19:33:10 +0200

    edit

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-let button_up = document.querySelector(".up")
+let button_up = document.querySelector(".up");
+let fixed = document.querySelector(".bottom-header");
+
+
+
 
 
 window.onscroll = function(){
-    if(this.scrollY>=1000){
-        button_up.classList.add("show")
-    }
-   else{
-       button_up.classList.remove("show")
-   }
+    
+       if(this.scrollY>=333.3333435058594){
+        fixed.classList.add("header-fixed");
+        }
+        else{
+        fixed.classList.remove("header-fixed");
+        }
+    
+    
+        if(this.scrollY>=1000){
+            button_up.classList.add("show");
+        }
+        else{
+           button_up.classList.remove("show");
+        }
+    
 }
+
 
 button_up.onclick= function(){
     window.scrollTo({
         top:0,
         behavior:"smooth"
     })
-}
-
-
-let fixed = document.querySelector(".bottom-header")
-
-window.onscroll= function(){
-    if(this.scrollY>=333.3333435058594){
-        fixed.classList.add("header-fixed")
-    }
-    else{
-        fixed.classList.remove("header-fixed")
-    }
-}
+};
